d6a281b2253d34b70ab68465942cc6b00a382d39
mesropy <lilit.mesropyan@gmail.com>
2023-01-21 16:47:11 -0500

    Create initial chat UI

--- rizzler/src/App.js
+++ rizzler/src/App.js
@@ -1,11 +1,66 @@
 import './App.css';
+import { v4 as uuid } from 'uuid';
+import React, { useState } from 'react';
+import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { 
+  MainContainer, 
+  ChatContainer, 
+  MessageList, 
+  Message, 
+  MessageInput, 
+  TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [typing, setTyping] = useState(false)
+
+  const onInputChange = (event) => {
+    setNewMessage(event);
+  };
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      if (newMessage.trim() !== "") {
+        const newMessageList = messageList.concat(newMessage);
+        setMessageList(newMessageList);
+      } 
+      setNewMessage('');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is a test lul</h1>
       </header>
+
+      <div style={{ position:"relative", height: "500px" }}>
+        <MainContainer>
+          <ChatContainer>   
+            <MessageList>
+              <Message model={{
+                        message: "Hello my friend",
+                        sentTime: "just now",
+                        sender: "Joe"
+                        }} />
+              {messageList.map((item) => (
+                <Message 
+                  key={uuid()}
+                  model={{
+                  message: item,
+                  sentTime: "just now",
+                  direction: "outgoing"
+                  }}/>
+              ))}
+              {typing && <TypingIndicator />}
+            </MessageList>
+            <MessageInput placeholder={/*newMessage === '' ? */ "Type message here" /*: newMessage*/}
+                          onFocus={(e) => e.target.placeholder = ""}
+                          onChange={onInputChange}
+                          onKeyDown={handleKeyDown}/>
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
